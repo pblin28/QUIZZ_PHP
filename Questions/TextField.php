2ec640813705ea699a42c720f2f3342b68d48c83
SNIVEAU <odevva@gmail.com>
2024-01-19 09:53:39 +0100

    implémentatoin troisième question

--- Questions/TextField.php
+++ Questions/TextField.php
@@ -3,12 +3,18 @@
 require_once('Question.php');
 
 class TextField extends Question {
+    public function __construct($uuid, $label, $correctAnswer) {
+        parent::__construct($uuid, $label);
+        $this->correctAnswer = $correctAnswer;
+    }
     public function display() {
         echo "<div class='question'>";
         echo "<h2>{$this->getLabel()}</h2>";
-        echo "<input type='text' name='{$this->getLabel()}' placeholder='Votre réponse'>";
+        echo "<input type='text' name='{$this->getUuid()}' placeholder='Votre réponse'>";
         echo "</div>";
     }
+    public function getCorrectAnswer() {
+        return $this->correctAnswer;
+    }
 }
-
 ?>
